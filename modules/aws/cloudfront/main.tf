41836e05692290caa564c9f823d1a6fc6b6833d3
michael.ekpang <michael.ekpang@flutterwavego.com>
2020-06-01 17:14:51 +0100

    testing cloudfront 3

--- modules/aws/cloudfront/main.tf
+++ modules/aws/cloudfront/main.tf
@@ -19,7 +19,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     prefix          = "cloudfront_logs"
   }
 
-  aliases = ["${var.domain}"]
+  aliases = ["${var.domain_name}"]
 
   default_cache_behavior {
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
