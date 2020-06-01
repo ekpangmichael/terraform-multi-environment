721d891d917de161ae16a1d9f48e67417d1cbd11
michael.ekpang <michael.ekpang@flutterwavego.com>
2020-06-01 12:55:30 +0100

    testing output3

--- dev/outputs.tf
+++ dev/outputs.tf
@@ -11,17 +11,17 @@
 # }
 
 
-# output "website_bucket_arn" {
-#   description = "ARN of the bucket"
-#   value       = module.s3.arn
-# }
+output "website_bucket_arn" {
+  description = "ARN of the bucket"
+  value       = module.s3.*.arn
+}
 
-# output "website_bucket_name" {
-#   description = "Name (id) of the bucket"
-#   value       = module.s3.name
-# }
+output "website_bucket_name" {
+  description = "Name (id) of the bucket"
+  value       = module.s3.*.name
+}
 
-# output "website_endpoint" {
-#   description = "Domain name of the bucket"
-#   value       = module.s3.website_endpoint
-# }
+output "website_endpoint" {
+  description = "Domain name of the bucket"
+  value       = module.s3.*.website_endpoint
+}
