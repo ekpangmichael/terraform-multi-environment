531748fd1c1889d77bd26d56d2473bd19cb37365
michael.ekpang <michael.ekpang@flutterwavego.com>
2020-06-06 21:10:56 +0100

    added eks module

--- dev/main.tf
+++ dev/main.tf
@@ -3,57 +3,119 @@
 provider "aws" {
   region = var.region
 }
+locals {
+  cluster_name = "tr1pp-dev-cluster"
+}
 
-# VPC MODULE
-# module "vpc" {
-#   source  = "../modules/aws/vpc/"
+#VPC MODULE
+module "vpc" {
+  source  = "../modules/aws/vpc/"
 
-#   name = var.vpc_name
-#   cidr = var.vpc_cidr
+  name = var.vpc_name
+  cidr = var.vpc_cidr
 
-#   azs             = var.vpc_azs
-#   private_subnets = var.vpc_private_subnets
-#   public_subnets  = var.vpc_public_subnets
+  azs             = var.vpc_azs
+  private_subnets = var.vpc_private_subnets
+  public_subnets  = var.vpc_public_subnets
 
-#   enable_nat_gateway = var.vpc_enable_nat_gateway
+  enable_nat_gateway = var.vpc_enable_nat_gateway
 
-#   tags = var.vpc_tags
+  #tags = var.vpc_tags
+
+    tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = "1"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = "1"
+  }
+}
+
+
+#####################################
+#         cluster                   #
+#####################################
+
+module "eks" {
+  source       = "terraform-aws-modules/eks/aws"
+  version = "12.0.0"
+  cluster_name = local.cluster_name
+  subnets      = module.vpc.private_subnets
+
+  tags = {
+    Environment = "training"
+    GithubRepo  = "terraform-aws-eks"
+    GithubOrg   = "terraform-aws-modules"
+  }
+
+  vpc_id = module.vpc.vpc_id
+
+  worker_groups = [
+    {
+      name                          = "worker-group-1"
+      instance_type                 = "t2.small"
+      additional_userdata           = "echo foo bar"
+      asg_desired_capacity          = 2
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+    },
+    {
+      name                          = "worker-group-2"
+      instance_type                 = "t2.medium"
+      additional_userdata           = "echo foo bar"
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
+      asg_desired_capacity          = 1
+    },
+  ]
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+
+# #S3 MODULE
+# module "s3" {
+#   source = "../modules/aws/s3/"
+#   bucket_name = var.bucket_name
 # }
 
-#S3 MODULE
-module "s3" {
-  source = "../modules/aws/s3/"
-  bucket_name = var.bucket_name
-}
+# module "s3_superadmin" {
+#   source = "../modules/aws/s3/"
+#   bucket_name = "superadmin-dev-infra"
+# }
+# # # ACM
+# # module "acm" {
+# #   source = "../modules/aws/acm/"
+# #   domain_name = var.domain_name
+# # }
 
-module "s3_superadmin" {
-  source = "../modules/aws/s3/"
-  bucket_name = "superadmin-dev-infra"
-}
-# # ACM
-# module "acm" {
-#   source = "../modules/aws/acm/"
+# #cloudfront
+# module "cloudfront" {
+#   source = "../modules/aws/cloudfront/"
+#   regional_domain_name = module.s3.bucket_regional_domain_name
+#   bucket_name = var.bucket_name
 #   domain_name = var.domain_name
+#   base_domain_name = var.base_domain_name
+#   aws_acm_certificate_arn = "arn:aws:acm:us-east-1:387883916874:certificate/1304a2ac-d98d-4e00-92fb-07668533d0bc"
 # }
 
-#cloudfront
-module "cloudfront" {
-  source = "../modules/aws/cloudfront/"
-  regional_domain_name = module.s3.bucket_regional_domain_name
-  bucket_name = var.bucket_name
-  domain_name = var.domain_name
-  base_domain_name = var.base_domain_name
-  aws_acm_certificate_arn = "arn:aws:acm:us-east-1:387883916874:certificate/1304a2ac-d98d-4e00-92fb-07668533d0bc"
-}
-
-module "cloudfront_superadmin" {
-  source = "../modules/aws/cloudfront/"
-  regional_domain_name = module.s3_superadmin.superadmin_bucket_regional_domain_name
-  bucket_name = "superadmin-dev-infra"
-  domain_name = "superadmin-dev.infra.tk"
-  base_domain_name = var.base_domain_name
-  aws_acm_certificate_arn = "arn:aws:acm:us-east-1:387883916874:certificate/1304a2ac-d98d-4e00-92fb-07668533d0bc"
-}
+# module "cloudfront_superadmin" {
+#   source = "../modules/aws/cloudfront/"
+#   regional_domain_name = module.s3_superadmin.superadmin_bucket_regional_domain_name
+#   bucket_name = "superadmin-dev-infra"
+#   domain_name = "superadmin-dev.infra.tk"
+#   base_domain_name = var.base_domain_name
+#   aws_acm_certificate_arn = "arn:aws:acm:us-east-1:387883916874:certificate/1304a2ac-d98d-4e00-92fb-07668533d0bc"
+# }
 # module "ec2_instances" {
 #   source  = "terraform-aws-modules/ec2-instance/aws"
 #   version = "2.12.0"
--- dev/variables.tf
+++ dev/variables.tf
@@ -1,10 +1,10 @@
 # VPC VARIABLES
 
-# variable "vpc_name" {
-#   description = "Name of VPC"
-#   type        = string
-#   default     = "example-vpc"
-# }
+variable "vpc_name" {
+  description = "Name of VPC"
+  type        = string
+  default     = "example-vpc"
+}
 
 variable "region" {
   description = "Name of region"
@@ -12,35 +12,35 @@ variable "region" {
   default = "eu-west-1"
 }
 
-# variable "vpc_cidr" {
-#   description = "CIDR block for VPC"
-#   type        = string
-#   default     = "10.0.0.0/16"
-# }
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
 
-# variable "vpc_azs" {
-#   description = "Availability zones for VPC"
-#   type        = list
-#   default     = ["us-west-2a", "us-west-2b", "us-west-2c"]
-# }
+variable "vpc_azs" {
+  description = "Availability zones for VPC"
+  type        = list
+  default     = ["us-west-2a", "us-west-2b"]
+}
 
-# variable "vpc_private_subnets" {
-#   description = "Private subnets for VPC"
-#   type        = list(string)
-#   default     = ["10.0.1.0/24", "10.0.2.0/24"]
-# }
+variable "vpc_private_subnets" {
+  description = "Private subnets for VPC"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
 
-# variable "vpc_public_subnets" {
-#   description = "Public subnets for VPC"
-#   type        = list(string)
-#   default     = ["10.0.101.0/24", "10.0.102.0/24"]
-# }
+variable "vpc_public_subnets" {
+  description = "Public subnets for VPC"
+  type        = list(string)
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+}
 
-# variable "vpc_enable_nat_gateway" {
-#   description = "Enable NAT gateway for VPC"
-#   type    = bool
-#   default = true
-# }
+variable "vpc_enable_nat_gateway" {
+  description = "Enable NAT gateway for VPC"
+  type    = bool
+  default = true
+}
 
 # variable "vpc_tags" {
 #   description = "Tags to apply to resources created by VPC module"
@@ -54,22 +54,22 @@ variable "region" {
 
 # S3 BUCKET VARIABLES
 
-variable "bucket_name" {
-  description = "S3 bucket name"
-  type        = string
-  default = "dev-infra"
-}
+# variable "bucket_name" {
+#   description = "S3 bucket name"
+#   type        = string
+#   default = "dev-infra"
+# }
 
-variable "domain_name" {
-  description = "domain name for acm"
-  type        = string
-}
+# variable "domain_name" {
+#   description = "domain name for acm"
+#   type        = string
+# }
 
-variable "base_domain_name" {
-  description = "domain name for acm"
-  type        = string
-   default = "infra.tk"
-}
+# variable "base_domain_name" {
+#   description = "domain name for acm"
+#   type        = string
+#    default = "infra.tk"
+# }
 
 # variable "name" {
 #   description = "domain name for acm"
