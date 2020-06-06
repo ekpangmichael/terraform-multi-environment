8a63816cd71234f9e89e57f5adac6d28a54eb983
michael.ekpang <michael.ekpang@flutterwavego.com>
2020-06-06 22:01:33 +0100

    change variables

--- dev/output.tf
+++ dev/output.tf
@@ -30,10 +30,10 @@ output "config_map_aws_auth" {
   value       = module.eks.config_map_aws_auth
 }
 
-output "region" {
-  description = "AWS region"
-  value       = var.region
-}
+# output "region" {
+#   description = "AWS region"
+#   value       = var.region
+# }
 
 output "cluster_name" {
   description = "Kubernetes Cluster Name"
