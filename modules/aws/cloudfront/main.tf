6466e1c9a2e1525b3a7b77f54084735dfacd4636
michael.ekpang <michael.ekpang@flutterwavego.com>
2020-06-09 21:39:47 +0100

    update super admin

--- modules/aws/cloudfront/main.tf
+++ modules/aws/cloudfront/main.tf
@@ -23,7 +23,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
     prefix          = "cloudfront_logs"
   }
 
-  aliases = ["${var.domain_name}","${var.wwwdomain}"]
+  aliases = ["${var.domain_name}"]
 
   default_cache_behavior {
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
